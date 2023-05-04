001122801d0ec40bd4148bc1c0cb7d2fd857c6d5
ugur-vaadin <ugur@vaadin.com>
2023-05-04 09:58:51 +0300

    refactor: update material styles based on extracted base styles

--- packages/side-nav/theme/material/vaadin-side-nav-styles.js
+++ packages/side-nav/theme/material/vaadin-side-nav-styles.js
@@ -6,23 +6,8 @@ import '@vaadin/vaadin-material-styles/font-icons.js';
 import { css, registerStyles } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
 
 export const sideNavItemStyles = css`
-  :host {
-    display: block;
-  }
-
-  [hidden] {
-    display: none !important;
-  }
-
   a {
-    flex: auto;
-    min-width: 0;
-    display: flex;
-    align-items: center;
     gap: 4px;
-    text-decoration: none;
-    color: inherit;
-    font: inherit;
     padding: 8px 4px;
     padding-inline-start: calc(0.5rem + var(--_child-indent, 0px));
     border-radius: 4px;
@@ -32,8 +17,6 @@ export const sideNavItemStyles = css`
   }
 
   button {
-    -webkit-appearance: none;
-    appearance: none;
     border: 0;
     margin: 0;
     margin-inline-end: -4px;
@@ -41,24 +24,10 @@ export const sideNavItemStyles = css`
     background: transparent;
     font: inherit;
     color: var(--material-secondary-text-color);
-    flex: none;
     width: 1.875rem;
     height: 1.875rem;
     cursor: default;
     transition: color 140ms;
-  }
-
-  :host(:not([path])) a {
-    position: relative;
-  }
-
-  :host(:not([path])) button::after {
-    content: '';
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
   }
 
   @media (any-hover: hover) {
@@ -106,18 +75,7 @@ export const sideNavItemStyles = css`
     color: var(--material-body-text-color);
   }
 
-  slot[name='prefix'],
-  slot[name='suffix'] {
-    flex: none;
-  }
-
   slot:not([name]) {
-    display: block;
-    flex: auto;
-    min-width: 0;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
     margin: 0 0.25rem;
   }
 
@@ -137,12 +95,6 @@ export const sideNavItemStyles = css`
     --_child-indent-2: var(--_child-indent);
   }
 
-  slot[name='children'] {
-    /* Needed to make role="list" work */
-    display: block;
-    width: 100%;
-  }
-
   :host([active]) a {
     color: var(--material-secondary-text-color);
     background-color: var(--material-secondary-background-color);
@@ -153,7 +105,6 @@ registerStyles('vaadin-side-nav-item', sideNavItemStyles, { moduleId: 'material-
 
 export const sideNavStyles = css`
   :host {
-    display: block;
     font-family: var(--material-font-family);
     font-size: var(--material-small-font-size);
     font-weight: 500;
@@ -162,41 +113,22 @@ export const sideNavStyles = css`
     -webkit-tap-highlight-color: transparent;
   }
 
-  [hidden] {
-    display: none !important;
-  }
-
   summary {
     cursor: default;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
     border-radius: 4px;
   }
 
   summary ::slotted([slot='label']) {
-    display: block;
     font-size: var(--material-caption-font-size);
     color: var(--material-secondary-text-color);
     margin: 0.5em;
     border-radius: inherit;
   }
 
-  summary::-webkit-details-marker {
-    display: none;
-  }
-
-  summary::marker {
-    content: '';
-  }
-
   summary::after {
     font-family: material-icons;
     color: var(--material-secondary-text-color);
     font-size: 1.5em;
-    display: inline-flex;
-    align-items: center;
-    justify-content: center;
     width: 1.875rem;
     height: 1.875rem;
     transition: transform 140ms;
@@ -225,11 +157,6 @@ export const sideNavStyles = css`
     summary:focus-visible {
       box-shadow: 0 0 0 2px var(--material-primary-color);
     }
-  }
-
-  slot {
-    /* Needed to make role="list" work */
-    display: block;
   }
 `;
 
